157066680fefdd3ad869d1d00107fe4e825187cc
UltraArcher <ultra.archerhm@gmail.com>
2024-07-23 16:01:51 +1000

    finished v1, idk how secure or efficient it is might try remapping the thing to it.

--- Password maker.py
+++ Password maker.py
@@ -1,5 +1,4 @@
 import os
-
 allchars = []
 upperletters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 lowerletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -11,3 +10,16 @@ maxsize = int(input("Maximum Length: "))
 allow_numbers = input("Allow numbers: ").lower() in ["yes", "true"]
 allow_special_characters = input("Allow special characters: ").lower() in ["yes", "true"]
 allow_capital_letters = input("Allow capital characters: ").lower() in ["yes", "true"]
+if allow_numbers:
+    allchars.extend(numbers)
+if allow_special_characters:
+    allchars.extend(special_characters)
+if allow_capital_letters:
+    allchars.extend(upperletters)
+
+allchars.extend(lowerletters)
+
+for _ in range(os.urandom(1)[0] % (maxsize - minsize + 1) + minsize):
+    password += allchars[os.urandom(1)[0] % len(allchars)]
+
+print(password)
